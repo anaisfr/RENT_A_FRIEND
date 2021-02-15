6d5eb77b8fd5691e0b30c9c7b1514c87d82a6938
Marko Glibota <markoglibota@gmail.com>
2021-02-15 18:08:33 +0100

    added has_one_attached photo to User and Friend models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :friends, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
   validates :gender, inclusion: { in: User::GENDER }
   validates :email, :password, :first_name, :last_name, :city, :gender, :age, presence: true
 end

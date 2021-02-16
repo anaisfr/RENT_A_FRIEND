eba17e17e47102f6b1999204070499a3068f26eb
Marko Glibota <markoglibota@gmail.com>
2021-02-16 12:12:38 +0100

    deleted status from strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,6 +19,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :status)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end

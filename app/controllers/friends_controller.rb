2c52d41a940a3dd9f202d67dcaccfa83394874e0
Julien Cromback <crombackjulien@gmail.com>
2021-02-16 11:25:22 +0100

    Controller friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,17 @@
 class FriendsController < ApplicationController
+  before_action :set_friend, only: [:show]
+
+  def index
+    @friends = Friend.all
+  end
+
+  def show
+	end
+
+	private
+
+	def set_friend
+    @friend = Friend.find(params[:id])
+  end
+
 end

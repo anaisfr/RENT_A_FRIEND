6fb36701a32ce14efca9f8c6aff3bffbc32c0fd4
Julien Cromback <crombackjulien@gmail.com>
2021-02-17 21:41:49 +0100

    Added a route canceled and a method canceled to controller bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -32,6 +32,12 @@ class BookingsController < ApplicationController
     redirect_to dashboard_path
   end
 
+  def canceled
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "canceled")
+    redirect_to dashboard_path
+  end
+
   private
 
   def booking_params

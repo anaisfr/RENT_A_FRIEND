468f993ea4d54b006669632a25ecbc92bc96e598
Marko Glibota <markoglibota@gmail.com>
2021-02-17 11:02:30 +0100

    modified booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
   resources :friends, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :bookings, only: [:index, :new, :create] do
-      member do
-        patch 'accepted'
-        patch 'denied'
-      end
-    end
+    resources :bookings, only: [:index, :new, :create]
   end
-
-
+  resources :bookings, only: [] do
+    member { patch 'accepted' }
+    member { patch 'denied' }
+  end
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
-
-
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

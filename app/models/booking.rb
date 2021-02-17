3c7207f91c7b55d9f9d45be8f1141692d311f7a4
Marko Glibota <markoglibota@gmail.com>
2021-02-17 14:35:03 +0100

    seed ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :friend
 
-  validates :user, presence: true, uniqueness: { scope: :friend }
+  validates :user, presence: true
   validates :friend, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true

c41e2fa9d1fc6fadb8c7488616488cf68b1a3d6d
Marko Glibota <markoglibota@gmail.com>
2021-02-18 16:28:19 +0100

    computing friend price

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,11 +25,11 @@ require("channels")
 // External imports
 import "bootstrap";
 import "../plugins/flatpickr"
+import { calendarDate } from '../plugins/date';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  calendarDate();
 });
---
+++ app/javascript/plugins/date.js
@@ -0,0 +1,29 @@
+const calendarDate = () => {
+  const date1 = document.getElementById("booking_start_date");
+  const date2 = document.getElementById("booking_end_date");
+
+
+
+  date2.addEventListener("change", (event) => {
+  if (date1.value) {
+
+    const startDate = new Date(date1.value);
+    const endDate = new Date(date2.value);
+
+
+    const dateDiff = endDate - startDate;
+
+    const dayDiff = dateDiff / 86400000;
+
+    const price = document.getElementById('price').innerText;
+
+    const total = document.querySelector('.total');
+
+    total.innerText = `${dayDiff * price}€`;
+
+    }
+  });
+};
+
+
+export { calendarDate };
